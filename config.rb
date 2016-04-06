1d4f6c9a317a9198962747c265c0e2bcc0cfa92f
iuunhao <html.5@foxmail.com>
2016-04-06 11:30:49 +0800

    add

---
+++ config.rb
@@ -0,0 +1,20 @@
+# 根目录
+http_path = '/'
+# sass路径
+sass_dir = 'sass'
+# 编译后css路径
+css_dir = 'css'
+# 图片路径
+images_dir = 'images'
+
+# 单行模式(测试推荐)
+output_style = :compact  
+# 压缩文件（上线推荐）
+# output_style = :compressed
+
+# 声明索引（建议开启）
+enable_sourcemaps = true
+sass_options = {:sourcemap => true}
+
+relative_assets = true
+line_comments = false
